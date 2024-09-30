6b5e59fb6ee3e46c93695963e14a4b003fdedfec
kimseongah <kimsa0322@naver.com>
2024-10-01 00:43:39 +0900

    Title: 행복한 수열의 개수, Time: 119ms, Memory: 27MB, Status: Passed - CodeTree

--- 241001/행복한 수열의 개수/number-of-happy-sequence.py
+++ 241001/행복한 수열의 개수/number-of-happy-sequence.py
@@ -7,27 +7,27 @@ for i in range(n):
     A[i] = list(map(int, input().split()))
 count = 0
 for i in range(n):
-    num_seq = 1
+    num_seq = [1]
     past_elem = A[i][0]
     for j in range(1, n):
         if past_elem == A[i][j]:
-            num_seq += 1
+            num_seq[-1] += 1
         else:
-            num_seq = 1
+            num_seq.append(1)
             past_elem = A[i][j]
-    if num_seq >= m:
+    if max(num_seq) >= m:
         count += 1
 
 for i in range(n):
-    num_seq = 1
+    num_seq = [1]
     past_elem = A[0][i]
     for j in range(1, n):
         if past_elem == A[j][i]:
-            num_seq += 1
+            num_seq[-1] += 1
         else:
-            num_seq = 1
+            num_seq.append(1)
             past_elem = A[j][i]
-    if num_seq >= m:
+    if max(num_seq) >= m:
         count += 1
 
 print(count)
