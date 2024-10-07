595e39cfeb7750f329749894ff3c8500e9d07911
kimseongah <kimsa0322@naver.com>
2024-10-07 15:47:11 +0900

    Title: 금 채굴하기, Time: 310ms, Memory: 27MB, Status: Passed - CodeTree

--- 241007/금 채굴하기/gold-mining.py
+++ 241007/금 채굴하기/gold-mining.py
@@ -6,14 +6,15 @@ A = [list(map(int, input().split())) for _ in range(n)]
 
 answer = 0
 # 가능한 가장 큰 마름모의 K == N-1
-for k in range(2*n-2):
+for k in range(2*n-1):
     cost = k*k + (k+1)*(k+1)
     for i in range(n):
         for j in range(n):
-            count = A[i][j]
+            count = 0
             for s in range(k+1):
                 for r in range(k-s+1):
                     if s == 0 and r == 0:
+                        count += A[i+s][j+r]
                         continue
                     if i+s >=0 and i+s < n and j+r>=0 and j+r <n :
                         count += A[i+s][j+r]
